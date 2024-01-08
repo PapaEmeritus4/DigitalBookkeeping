8bdba4557399f10974ff8d16ee1cb4396aefcf9f
PapaEmeritus4 <vovakaiman2005@gmail.com>
2024-01-08 19:21:44 +0200

    v0.0.6
    Add Hibernate to Project

--- src/main/java/org/example/dao/PersonDAO.java
+++ src/main/java/org/example/dao/PersonDAO.java
@@ -2,57 +2,82 @@ package org.example.dao;
 
 import org.example.models.Book;
 import org.example.models.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Component
 public class PersonDAO {
-    private final JdbcTemplate jdbcTemplate;
+//      РАБОТА С БД ЧЕРЕЗ Hibernate
+
+    private final SessionFactory sessionFactory;
 
     @Autowired
-    public PersonDAO(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public PersonDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
+    @Transactional(readOnly = true)
     public List<Person> index() {
-        return jdbcTemplate.query("SELECT * FROM Person",
-                new BeanPropertyRowMapper<>(Person.class));
+        Session session = sessionFactory.getCurrentSession();
+
+        List<Person> people = session.createQuery("SELECT Person from Person", Person.class)
+                .getResultList();
+
+        return people;
     }
 
-    public Person show(int id) {
-        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id},
-                new BeanPropertyRowMapper<>(Person.class))
-                .stream().findAny().orElse(null);
-    }
-
-    public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person(name, year_of_birth) VALUES (?, ?)",
-                person.getName(), person.getYearOfBirth());
-    }
-
-    public void update(int id, Person updatetPerson) {
-        jdbcTemplate.update("UPDATE Person SET name=?, year_of_birth=? WHERE id=?",
-                updatetPerson.getName(),
-                updatetPerson.getYearOfBirth(),
-                id);
-    }
-
-    public void delete(int id) {
-        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
-    }
-
-    public Optional<Person> getPersonByName(String name) {
-        return jdbcTemplate.query("SELECT * FROM Person WHERE name=?", new Object[]{name}, new BeanPropertyRowMapper<>(Person.class)).
-                stream().findAny();
-    }
-
-    public List<Book> getBooksByPersonId(int id) {
-        return jdbcTemplate.query("SELECT * FROM Book WHERE person_id=?", new Object[]{id},
-                new BeanPropertyRowMapper<>(Book.class));
-    }
 }
+//      РАБОТА С БД ЧЕРЕЗ JdbcTemplate
+
+//    private final JdbcTemplate jdbcTemplate;
+//
+//    @Autowired
+//    public PersonDAO(JdbcTemplate jdbcTemplate) {
+//        this.jdbcTemplate = jdbcTemplate;
+//    }
+//
+//    public List<Person> index() {
+//        return jdbcTemplate.query("SELECT * FROM Person",
+//                new BeanPropertyRowMapper<>(Person.class));
+//    }
+//
+//    public Person show(int id) {
+//        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id},
+//                new BeanPropertyRowMapper<>(Person.class))
+//                .stream().findAny().orElse(null);
+//    }
+//
+//    public void save(Person person) {
+//        jdbcTemplate.update("INSERT INTO Person(name, year_of_birth) VALUES (?, ?)",
+//                person.getName(), person.getYearOfBirth());
+//    }
+//
+//    public void update(int id, Person updatetPerson) {
+//        jdbcTemplate.update("UPDATE Person SET name=?, year_of_birth=? WHERE id=?",
+//                updatetPerson.getName(),
+//                updatetPerson.getYearOfBirth(),
+//                id);
+//    }
+//
+//    public void delete(int id) {
+//        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+//    }
+//
+//    public Optional<Person> getPersonByName(String name) {
+//        return jdbcTemplate.query("SELECT * FROM Person WHERE name=?", new Object[]{name}, new BeanPropertyRowMapper<>(Person.class)).
+//                stream().findAny();
+//    }
+//
+//    public List<Book> getBooksByPersonId(int id) {
+//        return jdbcTemplate.query("SELECT * FROM Book WHERE person_id=?", new Object[]{id},
+//                new BeanPropertyRowMapper<>(Book.class));
+//    }
+
--- src/main/java/org/example/models/Person.java
+++ src/main/java/org/example/models/Person.java
@@ -1,17 +1,26 @@
 package org.example.models;
 
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "Person")
 public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotEmpty(message = "Name must not be empty")
     @Size(min = 2, max = 200, message = "Name should be between 2 and 200 characters")
+    @Column(name = "name")
     private String name;
 
     @Min(value = 1900, message = "Year of birth should be greater than 1900")
+    @Column(name = "year_of_birth")
     private int yearOfBirth;
 
     public Person() {
